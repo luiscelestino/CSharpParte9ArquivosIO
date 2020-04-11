ee70ed0dbab530e28de0c29cc829d812bb5282a4
Luis Ricardo Celestino de Souza <souza.luis@gmail.com>
2020-04-11 10:56:38 -0300

    Update project

---
+++ ByteBankImportacaoExportacao/1_LidandoComStreamDiretamente.cs
@@ -0,0 +1,42 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankImportacaoExportacao
+{
+    partial class Program
+    {
+        static void LidandoComFileStreamDiretamente()
+        {
+            var enderecoDoArquivo = "contas.txt";
+
+            using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+            {
+                var buffer = new byte[1024]; // 1 KB
+                var numeroDeBytesLidos = -1;
+
+                while (numeroDeBytesLidos != 0)
+                {
+                    numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                    Console.WriteLine($"Bytes lidos: {numeroDeBytesLidos}");
+                    EscreverBuffer(buffer, numeroDeBytesLidos);
+                }
+            }
+        }
+
+        static void EscreverBuffer(byte[] buffer, int bytesLidos)
+        {
+            var encoding = new UTF8Encoding();
+            var texto = encoding.GetString(buffer, 0, bytesLidos);
+
+            Console.Write(texto);
+        }
+
+
+    }
+
+}
